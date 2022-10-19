a789315d495044fb5dc9172416102b55f33f7feb
Angel Perez <angel@lolabyte.local>
2022-10-19 17:03:39 -0400

    chore: Cleanup refactored code

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"os"
 
-	"github.com/dave/jennifer/jen"
-	"github.com/hashicorp/terraform-config-inspect/tfconfig"
 	"github.com/lolabyte/tf2go/gen"
 )
 
@@ -13,26 +11,6 @@ var (
 	outputPackageName string
 	outputDir         string
 )
-
-func tfVarToStructField(stmt *jen.Statement, v *tfconfig.Variable) *jen.Statement {
-	switch v.Type {
-	case "bool":
-		return stmt.Bool()
-	case "number":
-		return stmt.Int64()
-	case "string":
-		return stmt.String()
-	case "list(bool)":
-		return stmt.List(jen.Bool())
-	case "list(number)":
-		return stmt.List(jen.Int64())
-	case "list(string)":
-		return stmt.List(jen.String())
-	case "map(string)", "object":
-		return stmt.Map(jen.String()).String()
-	}
-	return nil
-}
 
 func main() {
 	err := gen.GenerateTFModulePackage(os.Args[1], os.Args[2], os.Args[3])
