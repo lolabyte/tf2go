88e3e971d5024424466d2b76f67de5c45b33c315
perangel <perangel@gmail.com>
2022-11-21 16:04:44 -0500

    test: update function signature

--- gen/generate_test.go
+++ gen/generate_test.go
@@ -9,13 +9,13 @@ import (
 
 func TestGenerateTFModulePackage(t *testing.T) {
 	t.Run("returns an error when the input module directory doesn't exist", func(t *testing.T) {
-		err := gen.GenerateTFModulePackage("does_not_exist", "out_dir", "test_module")
+		err := gen.GenerateTFModulePackage("does_not_exist", "out_dir", "test_module", "tf")
 		assert.Error(t, err)
 		assert.Equal(t, err.Error(), "Failed to read module directory: Module directory does_not_exist does not exist or cannot be read.")
 	})
 
 	t.Run("returns an error when input module is invalid", func(t *testing.T) {
-		err := gen.GenerateTFModulePackage("../testdata/invalid_tf_module", "out_dir", "test_module")
+		err := gen.GenerateTFModulePackage("../testdata/invalid_tf_module", "out_dir", "test_module", "tf")
 		assert.Error(t, err)
 		assert.Regexp(t, "Argument or block definition required:.*$", err.Error())
 	})
